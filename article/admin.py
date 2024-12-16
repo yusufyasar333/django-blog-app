1fa39ba762c25a95bdf2334689aecd01ed53a010
Yusuf Yaşar <yusufyasar567@gmail.com>
2024-12-16 18:59:58 +0300

    Fix line endings

---
+++ article/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Article,Comment
+
+# Register your models here.
+
+admin.site.register(Comment)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+
+    list_display = ["title","author","create_date"]
+    list_display_links = ["title","create_date"]
+    search_fields = ["title"]
+    list_filter =["create_date"]
+    class Meta:
+        model =Article
